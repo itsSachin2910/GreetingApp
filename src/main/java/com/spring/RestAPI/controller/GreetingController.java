aadf45ba3ce79999727040206dc8f327f88267d7
“Sachin” <sachinkumar30690@gmail.com>
2025-03-04 21:57:50 +0530

    UC7

---
+++ src/main/java/com/spring/RestAPI/controller/GreetingController.java
@@ -0,0 +1,50 @@
+package com.spring.RestAPI.controller;
+
+import com.spring.RestAPI.model.Greeting;
+import com.spring.RestAPI.service.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/greetings")
+public class GreetingController {
+
+    private final GreetingService greetingService;
+
+    @Autowired
+    public GreetingController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    // Create Greeting
+    @PostMapping
+    public Greeting createGreeting(@RequestParam(required = false) String firstName,
+                                   @RequestParam(required = false) String lastName) {
+        return greetingService.createGreeting(firstName, lastName);
+    }
+
+    // Get Greeting by ID
+    @GetMapping("/{id}")
+    public Optional<Greeting> getGreetingById(@PathVariable Long id) {
+        return greetingService.getGreetingById(id);
+    }
+
+    // Get All Greetings
+    @GetMapping
+    public List<Greeting> getAllGreetings() {
+        return greetingService.getAllGreetings();
+    }
+
+    // Edit Greeting
+    @PutMapping("/{id}")
+    public Greeting updateGreeting(@PathVariable Long id,
+                                   @RequestParam(required = false) String firstName,
+                                   @RequestParam(required = false) String lastName) {
+        return greetingService.updateGreeting(id, firstName, lastName);
+    }
+
+
+}
